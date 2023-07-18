045599fd506857cf2df6d7802c564a9b6162d8e9
mammen <jsmammen@gmail.com>
2023-07-18 12:03:25 +0530

    Reverted to bootstrap css

--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"embed"
-	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -18,49 +16,10 @@ type Film struct {
 //go:embed templates
 var templateFS embed.FS
 
-func main1() {
-	fmt.Println("Go app...")
-
-	// handler function #1 - returns the index.html template, with film data
-	h1 := func(w http.ResponseWriter, r *http.Request) {
-
-		tmpl := template.Must(template.ParseFS(templateFS,
-			"templates/index.html"))
-
-		films := map[string][]Film{
-			"Films": {
-				{Title: "The Godfather", Director: "Francis Ford Coppola"},
-				{Title: "Blade Runner", Director: "Ridley Scott"},
-				{Title: "The Thing", Director: "John Carpenter"},
-			},
-		}
-		tmpl.Execute(w, films)
-	}
-
-	// handler function #2 - returns the template block with the newly added film, as an HTMX response
-	h2 := func(w http.ResponseWriter, r *http.Request) {
-		//time.Sleep(1 * time.Second)
-		title := r.PostFormValue("title")
-		director := r.PostFormValue("director")
-		// htmlStr := fmt.Sprintf("<li class='list-group-item bg-primary text-white'>%s - %s</li>", title, director)
-		// tmpl, _ := template.New("t").Parse(htmlStr)
-		//tmpl := template.Must(template.ParseFiles("index.html"))
-		tmpl := template.Must(template.ParseFS(templateFS,
-			"templates/index.html"))
-		tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title, Director: director})
-	}
-
-	// define handlers
-	http.HandleFunc("/", h1)
-	http.HandleFunc("/add-film/", h2)
-	http.Handle("/static/", http.FileServer(http.Dir("/static/")))
-
-	log.Fatal(http.ListenAndServe(":8000", nil))
-
-}
-
 func main() {
 	r := gin.Default()
+
+	r.StaticFS("/static", http.Dir("static"))
 
 	r.GET("/", func(c *gin.Context) {
 		tmpl := template.Must(template.ParseFS(templateFS,
@@ -77,7 +36,7 @@ func main() {
 
 	})
 
-	r.POST("/add-film/", func(c *gin.Context) {
+	r.POST("/films", func(c *gin.Context) {
 
 		title := c.PostForm("title")
 		director := c.PostForm("director")
